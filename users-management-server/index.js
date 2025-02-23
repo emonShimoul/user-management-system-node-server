4ed17d8800353413196026f24e9a885c57e535c1
emonShimoul <emonshimoul@gmail.com>
2025-02-24 00:45:38 +0600

    create react form and post API and send data to the server

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
 
 const users = [
   { id: 1, name: "Shabana", email: "shabana@gmail.com" },
   { id: 2, name: "Bobita", email: "bobita@gmail.com" },
-  { id: 1, name: "Kobita", email: "kobita@gmail.com" },
+  { id: 3, name: "Kobita", email: "kobita@gmail.com" },
 ];
 
 app.get("/", (req, res) => {
@@ -14,6 +18,12 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+
+  console.log(req.body);
 });
 
 app.listen(port, () => {
