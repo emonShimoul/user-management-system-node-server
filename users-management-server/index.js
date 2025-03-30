b44ec417d97646d4c5a13b2c6b1ce63c56fc8e7f
emonShimoul <emonshimoul@gmail.com>
2025-03-30 23:30:02 +0600

    remove unnecessary codes

--- users-management-server/index.js
+++ users-management-server/index.js
@@ -23,12 +23,6 @@ app.get("/users", (req, res) => {
 
 app.post("/users", (req, res) => {
   console.log("post api hitting");
-  console.log(req.body);
-  const newUser = req.body;
-  newUser.id = users.length + 1;
-  users.push(newUser);
-  //   console.log(users);
-  res.send(newUser);
 });
 
 app.listen(port, () => {
